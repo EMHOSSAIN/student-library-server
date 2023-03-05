52f065e9dd407dcab60230bfc27b84bc426cac2f
EMHOSSAIN <me288715@gmail.com>
2023-03-05 21:34:42 +0600

    added new post API

--- Index.js
+++ Index.js
@@ -1,51 +1,50 @@
-const express =  require('express')
+const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 
 const app = express();
-const port = process.env.PORT||5000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json());
 
 
-
-const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASSWORD}@cluster0.lp0luap.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASSWORD}@cluster0.ah4ac9g.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- 
-async function run(){
-    try{
+
+
+async function run() {
+    try {
         const libraryCollectionData = client.db('libraryData').collection('user');
 
-      app.post('/user', async(req,res)=>{
-        const user = req.body;
-        const result = await libraryCollectionData.insertOne(user)
-        res.send(result)
-      })
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+             const result = await libraryCollectionData.insertOne(user)
+            res.send(result)
+        })
 
-     
+
 
 
     }
-    finally{
+    finally {
 
     }
 
 }
-run().catch(error=> console.log(error))
+run().catch(error => console.log(error))
 
 
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('student library sarver is going on')
 })
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`port is going on ${port}`)
 })
